fc7fd886cab015220e4abb6d4e3fdfae86ffdf83
axefromut3@rambler.ru <axefromut3@rambler.ru@6ee24262-c51e-600b-5e17-339182504217>
2014-04-15 13:32:46 +0000

    MassivePush

---
+++ CheckerAggregator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyChecker
+{
+    class CheckerAggregator
+    {
+        CheckerAggregator(IChecker[] Checkers, ISender Sender)
+        {
+        }
+    }
+}
---
+++ IChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyChecker
+{
+    enum ResultStatus
+    {
+        OK,
+        ERR,
+        WARNING
+    }
+    
+    struct CheckResult
+    {
+        ResultStatus Status;
+        String Message;
+    }
+    
+    
+    interface IChecker
+    {
+        CheckResult Check();
+    }
+}
