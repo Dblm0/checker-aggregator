e34cdd6ab1300d560f1ecbb7cb90385b46c5a8ec
axefromut3@rambler.ru <axefromut3@rambler.ru@6ee24262-c51e-600b-5e17-339182504217>
2014-05-26 11:04:02 +0000

    Добавлена реализация метода SendMsg класса EmailSender

--- trunk/EmailSender.cs
+++ trunk/EmailSender.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Mail;
+using System.Net;
 
 namespace MyChecker
 {
     class EmailSender:ISender
     {
+        private String _user;
+        private String _pass;
+        public EmailSender()
+        {
+            _user = "logmeinpls@mail.ru";
+            Console.WriteLine(String.Format("Введите пароль для {0}", _user));
+            _pass = Console.ReadLine();
+        }
         public void SendMsg(String Msg)
         {
+            try
+            {
+                SmtpClient client = new SmtpClient("smtp.mail.ru", 587);
+                client.Credentials = new NetworkCredential(_user, _pass);
+                client.EnableSsl = true;
+                MailMessage Mail = new MailMessage(_user,_user, "Attention!",Msg);
+                Mail.BodyEncoding = Encoding.GetEncoding("UTF-8");
+                Mail.SubjectEncoding = Encoding.GetEncoding("UTF-8");
+                client.Send(Mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
