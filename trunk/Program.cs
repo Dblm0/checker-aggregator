08da3c3cf505342bfa8111a5d0d8248dbcf5b442
axefromut3@rambler.ru <axefromut3@rambler.ru@6ee24262-c51e-600b-5e17-339182504217>
2014-05-26 20:10:57 +0000

    Форматирование кода в PingChecker.cs , EmailSender.cs Сырая реализация CheckerAggregator через System.Threading.Tasks

--- trunk/Program.cs
+++ trunk/Program.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 namespace MyChecker
 {
     class Program
     {
         static void Main(string[] args)
         {
+            EmailSender Sender = new EmailSender();
+            
+            List<IChecker> Checkers = new List<IChecker>();
+            Checkers.Add(new PingChecker("codeproject.com"));
+            Checkers.Add(new FreeSpaceChecker("C"));
+
+            CheckerAggregator Aggregator = new CheckerAggregator(Checkers, Sender);
+            Aggregator.Run();
         }
     }
 }
